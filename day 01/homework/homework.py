c31656360abeba6a30cad34e4b064aa000cd4413
nikoudzilauri <antidze75@mail.ru>
2024-06-27 16:18:22 +0400

    first commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,20 @@
+name = "niko"
+
+
+surname = "udzilauri" 
+
+age = 13
+
+height = 170.5
+
+print(name) 
+
+print( name + " " + surname + " " + str(age) + " " + str(height) )
+
+is_ugly =False
+
+knows_programing = True
+
+lives_in_batumi=True
+
+hate_dogs=False
---
+++ day 20/homework/homework1.py
@@ -0,0 +1,10 @@
+#რა უნდა დაწერო
+def text(log):
+# რა დაიწერება
+  print(log + " Refsnes")
+#რა გამოვა
+text("Emil")
+text("name")
+text("password")
+
+#მე დავწერე კომენტარი როგორც მივხდი
